d61ae14ab547a600a1baa6b25510fea0625db38d
ruben <ruben.suazo2@gmail.com>
2020-04-17 14:12:59 -0400

    close

--- index.js
+++ index.js
@@ -208,11 +208,15 @@ const artists = [
 (1) Name of the first artist in the array
 (2) Bio of the third artist in the array */
 
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+artists[8].name = "Vincent Van Gogh";
 
+console.log(artists[8].name);
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -223,10 +227,10 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(id, name) {
-    /* code here */
-  }
-  
-  /**
+  return `The artist at index ${id} is ${artists[0].name}`;
+}
+
+console.log(getArtistByIndex(0));
 
 
 /* Task 4: Create a function called `removeArtist` that takes two arguments:
@@ -237,20 +241,24 @@ function getArtistByIndex(id, name) {
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset.
 */
-function removeArtist(/*code here*/) {
-    /* code here */
-  }
-  
-  /**
+function removeArtist(artists, id) {
+  let newArtist = [...artists];
+  artists.splice(id, 1);
+  return newArtist;
+}
+console.log(removeArtist(artists, 0));
 
 
 /* Task 5: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born the 20th century (1800-1900) */
 
-function get20s(/* Code here */){
+function get20s(data){
+  let twenty = [];
+  for (let i = 0; i < artists.length; i++){
+    if (artists[i].years >= '1900')
+    twenty.push(artist[i].)
+    }
 
-    /* Code here */
 
-  }
 
 /* Task 6: Create a function called lotsOfArt() that takes artists as an argument and returns an array with names of artists who painted more than 100 paintings */
 
