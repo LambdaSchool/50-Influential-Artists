407a4a2155b3c8bbf07c2366bd118f093e1935bd
Shanna <hullaby@gmail.com>
2020-04-17 19:35:05 -0400

    I am so sorry that I was unable to finish todays project

--- index.js
+++ index.js
@@ -206,13 +206,16 @@ const artists = [
 /* Task 1: Practice accessing data above by console.log-ing following items:
 
 (1) Name of the first artist in the array
+ 
 (2) Bio of the third artist in the array */
+console.log(artists[0].name) 
+console.log(artists[2].bio) 
 
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
-
+artists.slice(8, 9)[0].name = 'Vincent Van Gogh'
+console.log(artists[8])
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -221,10 +224,12 @@ const artists = [
  * 
  * For example, if getArtistByIndex is invoked with the inventory and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
+ * 
 */
-function getArtistByIndex(id, name) {
-    /* code here */
+function getArtistByIndex(arr, index) {
+  return `The artist at index ${index} is ${arr[index].name}`
   }
+  console.log(getArtistByIndex(artists,6))
   
   /**
 
@@ -237,12 +242,18 @@ function getArtistByIndex(id, name) {
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset.
 */
-function removeArtist(/*code here*/) {
-    /* code here */
-  }
-  
-  /**
+// function removeArtist(arr, index) {
 
+//   artists.splice(artists, index)[0]
+// console.log(artists)
+// }
+
+  
+  
+function removeArtist(arr, index) {
+  artists.slice(artists, artists[index]);
+}
+console.log(removeArtist(artists[9]))
 
 /* Task 5: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born the 20th century (1800-1900) */
 
